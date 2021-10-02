bc0a375ec540434fb686e69be0a9f669ad7d931e
CHAN-YUWEI <aufanspruch@gmail.com>
2021-10-02 20:39:10 +0800

    feat: index,detail,search function update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
 const express = require('express')
 const exphbs = require('express-handlebars')
-const restaurants = require('./restaurant.json')
+// const restaurants = require('./restaurant.json')
 const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurantInfo')
 
 mongoose.connect('mongodb://localhost/restaurant-Info')
 
@@ -23,18 +24,29 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurants.results })
+  Restaurant.find()
+   .lean()
+   .then( restaurants => res.render('index', { restaurants }))
+   .catch( error => console.error(error))
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  let restaurant = restaurants.results.find( (restaurant) =>  { return restaurant.id.toString() === req.params.restaurant_id} )
-  res.render('show', { restaurant: restaurant })
+  let id = req.params.restaurant_id
+  Restaurant.findById(id)
+   .lean()
+   .then(restaurant => res.render('show', { restaurant }))
+   .catch(error => console.error(error))
 })
 
 app.get('/search', (req, res) => {
   let keyword = req.query.keyword.trim().toLowerCase()
-  let searchResults = restaurants.results.filter(restaurant => restaurant.name.trim().toLowerCase().includes(keyword) || restaurant.category.trim().toLowerCase().includes(keyword))
-  res.render('index', {restaurants: searchResults})  
+  Restaurant.find({ $or: [
+    {name: keyword},
+    {category: keyword},
+  ]})
+  .lean()
+  .then( restaurants => res.render('index', { restaurants }))
+  .catch( error => console.error(error))
 })
 
 app.listen(port, () => {
